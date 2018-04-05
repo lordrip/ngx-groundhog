3b9b7d326228359a6b80252727b1fa3519e889c3
Martinez <ricardo.martinez@tag009442087150.clients.dynatrace.org>
2018-04-05 18:01:54 +0200

    Implementing internal Zebra style

--- src/lib/table/table.ts
+++ src/lib/table/table.ts
@@ -1,17 +1,17 @@
 /**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
+* @license
+* Copyright Google LLC All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://angular.io/license
+*/
 
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation, Renderer2, IterableDiffers, ChangeDetectorRef, ElementRef, Attribute} from '@angular/core';
 import {CDK_TABLE_TEMPLATE, CdkTable} from '@angular/cdk/table';
 
 /**
- * Wrapper for the CdkTable with Material design styles.
- */
+* Wrapper for the CdkTable with Material design styles.
+*/
 @Component({
   moduleId: module.id,
   selector: 'gh-table, table[gh-table]',
@@ -24,4 +24,31 @@ import {CDK_TABLE_TEMPLATE, CdkTable} from '@angular/cdk/table';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GhTable<T> extends CdkTable<T> { }
+export class GhTable<T> extends CdkTable<T> {
+  constructor(_differs: IterableDiffers,
+    _changeDetectorRef: ChangeDetectorRef,
+    _elementRef: ElementRef,
+    @Attribute('role') role: string,
+    private renderer2: Renderer2) {
+
+    super(_differs, _changeDetectorRef, _elementRef, role);
+  }
+
+  renderRows() {
+    super.renderRows();
+    this.renderZebraStyle();
+  }
+
+  renderZebraStyle() {
+    // console.log(this._rowPlaceholder.viewContainer);
+
+    for (let i=0; i < this._rowPlaceholder.viewContainer.length; i++) {
+      const elem = this._rowPlaceholder.viewContainer.get(i)!;
+
+      console.log(elem);
+      // this.renderer2.addClass(elem, 'dynamicClass');
+    }
+
+
+  }
+}
